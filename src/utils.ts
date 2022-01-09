4f1b11352d6348369435f12f8e1c11a24b2a693c
GeertvanDrunen <36422553+GeertvanDrunen@users.noreply.github.com>
2022-01-09 17:19:06 +0100

    rm

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import { Dimensions, Platform, StatusBar } from "react-native";
 
-export function getDeviceHeight(statusBarTranslucent: boolean | undefined):number {
+export function getDeviceHeight(
+  statusBarTranslucent: boolean | undefined
+): number {
   var height = Dimensions.get("window").height;
 
   if (Platform.OS === "android" && !statusBarTranslucent) {
-
-    return StatusBar.currentHeight ?  height - StatusBar.currentHeight : height;
+    return StatusBar.currentHeight ? height - StatusBar.currentHeight : height;
   }
 
   return height;
@@ -14,15 +15,25 @@ export function getDeviceHeight(statusBarTranslucent: boolean | undefined):numbe
 export const getElevation = (elevation: number) => {
   return {
     elevation,
-    shadowColor: "black",
-    shadowOffset: { width: 0.3 * elevation, height: 0.5 * elevation },
-    shadowOpacity: 0.2,
-    shadowRadius: 0.7 * elevation,
+    shadowColor: "transparent",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0,
+    shadowRadius: 0,
   };
 };
 
-export const SUPPORTED_ORIENTATIONS: ("portrait" | "portrait-upside-down" | "landscape" | "landscape-left" | "landscape-right")[] = [
-  "portrait", "portrait-upside-down", "landscape", "landscape-left", "landscape-right"
+export const SUPPORTED_ORIENTATIONS: (
+  | "portrait"
+  | "portrait-upside-down"
+  | "landscape"
+  | "landscape-left"
+  | "landscape-right"
+)[] = [
+  "portrait",
+  "portrait-upside-down",
+  "landscape",
+  "landscape-left",
+  "landscape-right",
 ];
 
 export const waitAsync = (ms: number): Promise<null> =>
